d7c3717d372d9f9d470f70397511d24f9eb0b85a
Tony Santos <santos.tony@gmail.com>
2020-01-15 08:19:26 -0500

    added new methods

--- Untitled-2.py
+++ Untitled-2.py
@@ -20,15 +20,34 @@
 # process_subquery - called when ( found between FROM and WHERE. pass location of '('
 # join_lists
 
-def get_select(sql_str, start_pos=0):
-    select_position = sql_str.upper().find('SELECT',beg=start_pos)
+from datetime import datetime
+from loguru import logger
+import sys
+import string
+
+
+
+datetime_format = "%Y%m%d-%H%M"
+logfile_datetime = datetime.now().strftime(datetime_format)
+
+logger.add(f"output-{sys.argv[0]}-{logfile_datetime}.log", backtrace=True, diagnose=True)
+
+def get_select_position(sql_str, start_pos=0):
+    select_position = sql_str.upper().find('SELECT', start_pos)
+    if sql_str[select_position + 6] in string.whitespace: # we matched keyword and not a substring (unless string ends with select and is followed by whitespace)
+        pass
+    else:
+        select_position = get_select_position(sql_str, select_position+6) # we matched a substring. keep looking
+        
     logger.info(f"select_position = {select_position}")
+    logger.info(f"character after 'SELECT':{sql_str[select_position + 6]}:")
+    logger.info(f"whitespace after SELECT: {sql_str[select_position + 6] in string.whitespace}")
     if select_position > -1:
         logger.info(f"before SELECT: {sql_str[:select_position]}")
 
     return select_position
 
-def get_matching_from_postion(sql_str, start_pos=0):
+def get_matching_from_position(sql_str, start_pos=0):
     sql_str = sql_str.upper()
     select_level = 1
     matching_from_position = -1
@@ -39,34 +58,37 @@ def get_matching_from_postion(sql_str, start_pos=0):
         elif token== 'FROM':
             select_level = select_level - 1
             logger.info(f"FROM found. select_level: {select_level}")
+
             if select_level == 0:
                 matching_from_position = index
                 break
-    logger.info(f"paren_level: {paren_level}")
+    logger.info(f"paren_level: {select_level}")
 
-    return matching_paren_position
+    return matching_from_position
+
+def junk():
     sql_str = sql_str.upper()
-    from_postion.find('FROM', beg=start_pos)
+    from_postion.find('FROM', start_pos)
     logger.info(f"from_position: {from_position}")
     if from_position > -1:
-        logger.info("before FROM: {from_postion}")
-        select_position = sql_str.find('SELECT', beg=0, end=from_position)
+        logger.info(f"before FROM: {from_postion}")
+        select_position = sql_str.find('SELECT', 0, from_position)
         if select_position > -1:
             logger.info(f"SELECT found in field list at position: {sql_str}. probable subquery")
             logger.info(f"sql_str: {sql_str[:from_position]}")
 
-def get_from_postion(sql_str, start_pos=0):
+def get_from_position(sql_str, start_pos=0):
     sql_str = sql_str.upper()
-    from_postion.find('FROM', beg=start_pos)
+    from_position = sql_str.find('FROM', start_pos)
     logger.info(f"from_position: {from_position}")
     if from_position > -1:
-        logger.info("before FROM: {from_postion}")
-        select_position = sql_str.find('SELECT', beg=0, end=from_position)
+        logger.info(f"before FROM: {from_position}")
+        select_position = sql_str.find('SELECT', 0, from_position)
         if select_position > -1:
             logger.info(f"SELECT found in field list at position: {sql_str}. probable subquery")
             logger.info(f"sql_str: {sql_str[:from_position]}")
 
-def get_matching_paren(sql_str, open_paren_position=0):
+def get_matching_paren_position(sql_str, open_paren_position=0):
     paren_level = 1
     matching_paren_position = -1
     for char, index in enumerate(sql_str):
@@ -85,20 +107,20 @@ def get_matching_paren(sql_str, open_paren_position=0):
 
 
 if __name__ == "__main__":
-    query1 = "SELECT col1, col2, col3 FROM tab1"
+    query1 = "SELECT    col1, col2, col3 FROM tab1"
     logger.info(f"query: {query1}")
-    logger.info(f"get_select(query1): {get_select(query1)}")
-    logger.info(f"get_from(query1): {get_from(query1)}")
+    logger.info(f"get_select_position(query1): {get_select_position(query1)}")
+    logger.info(f"get_from_position(query1): {get_from_position(query1)}")
  
-    query2 = "SELECT col1, col2, col3 FROM (SELECT col1, col2, col3 FROM tab1)"
+    query2 = "SELECT\ncol1, col2, col3 FROM (SELECT col1, col2, col3 FROM tab1)"
     logger.info(f"query: {query2}")
-    logger.info(f"get_select(query2): {get_select(query2)}")
-    logger.info(f"get_from(query2): {get_from(query2)}")
-    logger.info(f"get_matching_from(query2): {get_matching_from(query2)}")
+    logger.info(f"get_select_position(query2): {get_select_position(query2)}")
+    logger.info(f"get_from_postion(query2): {get_from_position(query2)}")
+    logger.info(f"get_matching_from_position(query2): {get_matching_from_position(query2)}")
     
     query3 = "SELECT col1, col2, col3 FROM (SELECT col1, col2, col3 FROM tab1) WHERE col1 < 10"
     logger.info(f"query: {query3}")
-    logger.info(f"get_select(query3): {get_select(query3)}")
-    logger.info(f"get_from(query3): {get_from(query3)}")
-    logger.info(f"get_matching_from(query3): {get_matching_from(query3)}")
+    logger.info(f"get_select_position(query3): {get_select_position(query3)}")
+    logger.info(f"get_from_position(query3): {get_from_position(query3)}")
+    logger.info(f"get_matching_from_position(query3): {get_matching_from_position(query3)}")
     
